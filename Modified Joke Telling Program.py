e2d9a8bc5155046f451c896594c6e96449de8e2d
Alex deCaussin-Webb <alexdecaussin@gmail.com>
2018-10-08 00:48:50 -0400

    Add files via upload

---
+++ Modified Joke Telling Program.py
@@ -0,0 +1,85 @@
+# - WARNING: THE FINAL JOKE IS GOING TO SPAM YOUR WINDOW WITH A PRINT FUNCTION :) -
+#Strings & Variables 1
+a = ('What')
+b = ('What?')
+c = ('What.')
+d = ('what')
+e = ('what?')
+f = ('what.')
+
+#Strings & Variables 2
+g = ('Who\'s there?')
+h = ('Who is there?')
+i = ('Who\'s there')
+j = ('Who is there')
+k = ('who\'s there?')
+l = ('who is there')
+m = ('who\'s there')
+
+#Loop/Joke 1
+UserInputs = 0
+
+while UserInputs < 3:
+    print('What do you get when you cross a snowman with a vampire?')
+    z = input()
+
+    UserInputs = UserInputs + 1
+
+    if z == a or z == b or z == c or z == d or z == e or z == f:
+        break
+
+else:
+    print('You should probably have asked \'What?\'. If you did, next time try typing it the same way you saw in the previous sentence.')
+    print('Better luck next time!')
+
+if z == a or z == b or z == c or z == d or z == e or z == f:
+     print('Frostbite!')
+
+print()
+print()
+
+#Loop/Joke 2
+UserInputs = 0
+
+while UserInputs < 3:
+    print('What do dentists call an astronaut\'s cavity?')
+    z = input()
+
+    UserInputs = UserInputs + 1
+
+    if z == a or z == b or z == c or z == d or z == e or z == f:
+        break
+
+else:
+    print('You should probably have asked \"What?\". If you did, next time try typing it the same way you saw in the previous sentence.')
+    print('Better luck next time!')
+
+if z == a or z == b or z == c or z == d or z == e or z == f:
+    print('A black hole!')
+
+print()
+print()
+
+#Loop/Joke 3
+UserInputs = 0
+
+while UserInputs < 3:
+    print('Knock knock.')
+    z = input()
+
+    UserInputs = UserInputs + 1
+
+    if z == g or z == h or z == i or z == j or z == k or z == l or z == m:
+        break
+
+else:
+    print('You should probably have asked "Who\'s there?\". If you did, next time try typing it the same way you saw in the previous sentence.')
+    print('Better luck next time!')
+
+if z == g or z == h or z == i or z == j or z == k or z == l or z == m:
+          print('Interrupting cow.')
+
+UserInputs = 0
+
+while UserInputs == 0:
+          print('-MOO!')
