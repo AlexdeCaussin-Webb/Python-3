ae71b6c2eeae8bfded55fae8a6288e5ac4a00e40
Alex deCaussin-Webb <alexdecaussin@gmail.com>
2018-10-08 00:03:28 -0400

    Find the Area & Perimeter of a Rectangle
    Does what it says on the lid.  A simple program to calculate the area & perimeter of a rectangle.

---
+++ Area & Perimeter of a Rectangle.py
@@ -0,0 +1,13 @@
+#This program calculates the perimeter and area of a rectangle.
+
+print('Hello. I can calculate the perimeter and area of a rectangle for you.')
+
+l = int(input('Enter the length: '))
+w = int(input('Enter the width: '))
+
+area = l*w
+perimeter = l+l+w+w
+
+print('All right, the perimeter is',perimeter,', and the area is',area,'.')
+
+
